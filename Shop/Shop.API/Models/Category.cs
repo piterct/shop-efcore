d2f88adf1a6bb9364915cc9af443a1b5df433a78
Michael Peter <michael_piterct@hotmail.com>
2021-06-26 17:54:01 -0300

    Category Model

---
+++ Shop/Shop.API/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.API.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(60, ErrorMessage = "Maximum characters  is 60")]
+        [MinLength(3, ErrorMessage = "Minimum characters  is 3")]
+        public string Title { get; set; }
+    }
+}
