9ee197cb13fd029fc20a99c4539e3d28b80b5ffa
Michael Peter <michael_piterct@hotmail.com>
2021-06-27 13:22:42 -0300

    User Model

---
+++ Shop/Shop.API/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.API.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [MaxLength(20, ErrorMessage = "Maximum characters  is 20")]
+        [MinLength(3, ErrorMessage = "Minimum characters  is 3")]
+        public string Username { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "Maximum characters  is 20")]
+        [MinLength(3, ErrorMessage = "Minimum characters  is 3")]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
