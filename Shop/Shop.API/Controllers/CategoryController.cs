0d79f33776a0d3930adf87ad3ba47ff6a3ada3ee
Michael Peter <michael_piterct@hotmail.com>
2021-07-03 00:04:47 -0300

    Task and ActionResult

--- Shop/Shop.API/Controllers/CategoryController.cs
+++ Shop/Shop.API/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Shop.API.Controllers
 {
@@ -8,43 +10,43 @@ namespace Shop.API.Controllers
     {
         [HttpGet]
         [Route("")]
-        public string Get()
+        public async Task<ActionResult<List<Category>>> Get()
         {
-            return "Hello World!";
+            return new List<Category>();
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public string GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int id)
         {
-            return "GET " + id.ToString();
+            return new Category();
         }
 
         [HttpPost]
         [Route("")]
-        public Category Post([FromBody] Category model)
+        public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
         {
-            return model;
+            return Ok(model);
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public Category Put(int id, [FromBody] Category model)
+        public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
         {
             if (model.Id == id)
             {
-                return model;
+                return Ok(model);
 
             }
 
-            return null;
+            return NotFound();
         }
 
         [HttpDelete]
         [Route("{id:int}")]
-        public string Delete()
+        public async Task<ActionResult<List<Category>>> Delete()
         {
-            return "DELETE";
+            return Ok();
         }
     }
 }
