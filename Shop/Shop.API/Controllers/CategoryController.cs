7c398dde069a2196189d653e79c8444f63be7ef7
Michael Peter <michael_piterct@hotmail.com>
2021-07-02 23:05:47 -0300

    Using RestFull

--- Shop/Shop.API/Controllers/CategoryController.cs
+++ Shop/Shop.API/Controllers/CategoryController.cs
@@ -5,8 +5,30 @@ namespace Shop.API.Controllers
     [Route("categories")]
     public class CategoryController : ControllerBase
     {
+        [HttpGet]
         [Route("")]
-        public string MyMethod()
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "Hello World!";
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "Hello World!";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
         {
             return "Hello World!";
         }
