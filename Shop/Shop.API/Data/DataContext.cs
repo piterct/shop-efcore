e4d0111c14c6a540b71f7647a0055a75d812123b
Michael Peter <michael_piterct@hotmail.com>
2021-07-04 10:48:42 -0300

    DataContext

---
+++ Shop/Shop.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.API.Models;
+
+namespace Shop.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
