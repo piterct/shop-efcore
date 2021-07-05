b79fd9eb91192758a2b93dc4d54a7677c8d9b499
Michael Peter <michael_piterct@hotmail.com>
2021-07-05 00:39:21 -0300

    controller product

---
+++ Shop/Shop.API/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.API.Data;
+using Shop.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shop.API.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.AsNoTracking().ToListAsync();
+            return Ok(products);
+        }
+    }
+}
