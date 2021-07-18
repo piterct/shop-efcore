29451d4627f0b2990fde9e61da893fae35c9e2f3
Michael Peter <michael_piterct@hotmail.com>
2021-07-18 12:19:46 -0300

    TokenService

---
+++ Shop/Shop.API/Services/TokenService.cs
@@ -0,0 +1,17 @@
+﻿using Shop.API.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.API.Services
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+        }
+    }
+}
