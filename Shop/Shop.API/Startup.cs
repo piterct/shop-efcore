6d76b54a9214321053f3d05792c8833da2ebe09f
Michael Peter <michael_piterct@hotmail.com>
2021-07-26 22:20:46 -0300

    Compression

--- Shop/Shop.API/Startup.cs
+++ Shop/Shop.API/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Shop.API.Data;
+using System.Linq;
 using System.Text;
 
 namespace Shop.API
@@ -23,6 +25,13 @@ namespace Shop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes =
+                ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+            });
+
             services.AddControllers();
 
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
