50bafb68d6f776dbb16ab1ccfcc4a3ca02b42691
Michael Peter <michael_piterct@hotmail.com>
2021-07-30 16:13:33 -0300

    AddCors

--- Shop/Shop.API/Startup.cs
+++ Shop/Shop.API/Startup.cs
@@ -25,6 +25,7 @@ namespace Shop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddResponseCompression(options =>
             {
                 options.Providers.Add<GzipCompressionProvider>();
